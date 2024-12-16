e08dd240b4d1340b573c6dfbcacb06f6a63c6607
chuluq <chuluq@arvis.id>
2024-12-16 08:21:44 +0700

    feat: learn assets

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
     <Alert />
+    <p>main page</p>
+    <img src="/logo.png" alt="logo" />
+    <img src="~/assets/emoji1.png" alt="emoji1" class="size-12" />
   </div>
 </template>
